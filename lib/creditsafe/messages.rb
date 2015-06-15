25d91b906316a917c15cc5aa0d0b4d2792a80d43
Pete Hamilton <peterejhamilton@gmail.com>
2015-06-15 12:50:04 +0100

    Add comment to for_code mmessage method

--- lib/creditsafe/messages.rb
+++ lib/creditsafe/messages.rb
@@ -61,6 +61,9 @@ module Creditsafe
       DATA_SERVICE_INVALID_RESPONSE
     ].freeze
 
+    # Creditsafe documentation shows a 6 digit error code, however their API
+    # strips the leading 0. To comply with the docs, we pad the API code here to
+    # ensure we find the right match
     def self.for_code(code)
       padded_code = code.rjust(6, '0')
       message = ALL.find { |msg| msg.code == padded_code }
