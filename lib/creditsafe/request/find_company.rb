4f5f339f6babf92df33e400b5a3ff22d8df40941
pieper126 <stijnbijnen@hotmail.com>
2017-04-19 13:02:58 +0200

    Changes search type to MatchWords

--- lib/creditsafe/request/find_company.rb
+++ lib/creditsafe/request/find_company.rb
@@ -17,7 +17,7 @@ module Creditsafe
         search_criteria = {}
 
         search_criteria["#{Creditsafe::Namespace::DAT}:Name"] = {
-          '@MatchType' => 'MatchBeginning',
+          '@MatchType' => 'MatchWords',
           :content! => company_name
         } unless company_name.nil?
 
