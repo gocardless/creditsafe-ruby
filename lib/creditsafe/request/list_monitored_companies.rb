4b888574a187df2dd02fb0e7b120fce8ee84bea6
pieper126 <stijn.bijnen@invoicefinance.nl>
2017-05-15 15:47:55 +0200

    Fixes style in list_monitored_companies

--- lib/creditsafe/request/list_monitored_companies.rb
+++ lib/creditsafe/request/list_monitored_companies.rb
@@ -1,9 +1,17 @@
+# frozen_string_literal: true
+
 require 'creditsafe/namespace'
 
 module Creditsafe
   module Request
     class ListMonitoredCompanies
-      def initialize(portfolio_ids, first_position, page_size, changed_since, changed_only)
+      def initialize(
+        portfolio_ids,
+        first_position,
+        page_size,
+        changed_since,
+        changed_only
+      )
         @portfolio_ids = portfolio_ids
         @first_position = first_position
         @page_size = page_size
@@ -14,12 +22,12 @@ module Creditsafe
       def message
         message = {
           "#{Creditsafe::Namespace::OPER}:portfolioIds" => [
-            "#{Creditsafe::Namespace::ARR}:unsignedInt"=> @portfolio_ids
+            "#{Creditsafe::Namespace::ARR}:unsignedInt" => @portfolio_ids
           ],
           "#{Creditsafe::Namespace::OPER}:changedOnly" => @changed_only,
           "#{Creditsafe::Namespace::OPER}:changedSince" => @changed_since,
           "#{Creditsafe::Namespace::OPER}:pageSize" => @page_size,
-          "#{Creditsafe::Namespace::OPER}:firstPosition" => @first_position,
+          "#{Creditsafe::Namespace::OPER}:firstPosition" => @first_position
         }
 
         message
